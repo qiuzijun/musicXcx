03b8942ec6679de8c2455d1d2ed041701b8e0904
qiuzijun <3118897593@qq.com>
2021-07-16 22:33:27 +0800

    7.16-22:33

--- app.js
+++ app.js
@@ -1,39 +1,128 @@
+// const { get } = require("http");
+const regeneratorRuntime = require('./utils/runtime');
 // app.js
 App({
   globalData:{
-    musicList:[]
+    // musicBs:[]
   },
-  onLaunch() {
+  onLaunch() {   // 获取推荐歌曲
     let that = this;
-    const requestTask = wx.request({
+    let musicBs = []
+    const requestTask1 = wx.request({
       url: 'http://192.168.0.101:3000/personalized/newsong',
       data:{
+        // 前100条
         limit:100
       },
       success(res){
-        console.log(res.data.result);
+        // console.log(res.data.result);
+        // 本地缓存歌曲
         try {
-          wx.setStorageSync('gedan', res.data.result)
+          wx.setStorageSync('tjGequ', res.data.result)
         } catch (error) {
           console.log(error);
         }
-        
       },
       fail(error){
         console.log(error);
       }
     })
-    
+    // 获取热门歌单
+    const requestTask2 = wx.request({
+      url:"http://192.168.0.101:3000/top/playlist",
+      data:{
+        // 前6条热单
+        limit:6,
+        order:'hot'
+      },
+      success(res){
+      // console.log(res.data.playlists);
+      // 缓存本地
+            try {
+              wx.setStorageSync('remGedan', res.data.playlists)
+            } catch (error) {
+              console.log(error);
+            }
+      }
+    })
+    // 官方歌单
+    const requestTask3 = wx.request({
+      url:"http://192.168.0.101:3000/top/playlist/highquality",
+      data:{
+        // 前6条热单
+        limit:6,
+        before:1503639064232
+      },
+      success(res){
+      console.log(res);
+      // 缓存本地
+            try {
+              wx.setStorageSync('guanFang', res.data.playlists)
+            } catch (error) {
+              console.log(error);
+            }
+      }
+    })
+    // 歌曲排行
+    const requestTask4 = wx.request({
+      url:"http://192.168.0.100:3000/toplist",
+      success(res){
+        // 获取榜单ID
+      // console.log(res.data.list);
+      for (let i = 0; i < 2; i++) {
+        // console.log(res.data.list[i].id);
+        if (i == 0) {
+          // 飙升榜
+          wx.request({
+            url: 'http://192.168.0.100:3000/playlist/detail',
+            data:{
+              id:res.data.list[i].id
+            },
+          async  success(res){
+              // console.log(res.data.privileges)
+              for (let i = 0; i < res.data.privileges.length; i++) {
+                // console.log(res.data.privileges[i].id);
+                // 飙升榜歌曲信息
+                let music  =await  wx.request({
+                      url: 'http://192.168.0.100:3000/song/detail',
+                      data:{
+                        ids:res.data.privileges[i].id
+                      },
+                    })
+                    console.log(music);
+              }
+              try {
+                  wx.setStorageSync('soaringList', musicBs)
+              } catch (error) {
+                
+              }
+            }
+          })
+        }else{
+          // 新歌榜
+          wx.request({
+            url: 'http://192.168.0.101:3000/playlist/detail',
+            data:{
+              id:res.data.list[i].id
+            },
+            success(res){
+              console.log(res.data.privileges);
+            }
+          })
+        }
+      }
+      }
+    })
   },
   globalData: {
     username:null
   },
   onPageNotFound(){
     wx.switchTab({
-      url: 'pages/logs/logs',
+      url: 'pages/logs/logs', 
     })
   },
   onLoad(options){
-    console.log(this.globalData.username);
+    // console.log(this.globalData.username);
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,69 @@
 
 Page({
   data: {
-    musicList:[]
+    // 歌曲推荐
+    musicList:[],
+    // 歌单
+    musicHot:[],
+    // 官方
+    musicGf:[]
   },
   onLoad(){
     let that = this;
     try {
-     var value =  wx.getStorageSync('gedan')
+      // 获取本地缓存歌曲
+     var value =  wx.getStorageSync('tjGequ')
      if(value){
       that.setData({
-        musicList:value  
-                                             
+        musicList:value                                   
       })
      }
     } catch (error) {
       console.log(error);
     }
-    console.log(that.data.musicList);
+    try {
+      // 获取本地缓存歌单
+     var hot =  wx.getStorageSync('remGedan')
+     if(hot){
+       
+      that.setData({
+        musicHot:hot                                   
+      })
+     }
+    } catch (error) {
+      console.log(error);
+    }
+    // console.log(that.data.musicHot);
+    try {
+      // 获取本地缓存歌单
+     var gf =  wx.getStorageSync('guanFang')
+     if(gf){
+      that.setData({
+        musicGf:gf                                   
+      })
+     }
+    } catch (error) {
+      console.log(error);
+    }
   },
+  
+  // 跳转播放页面
   play(){
     wx.navigateTo({
       url: '../play/play',
     })
+  },
+  // 跳转歌单歌曲页面
+  hotGe(){
+    wx.navigateTo({
+      url: '../hotGdGequ/hotGdGequ',
+    })
+  },
+  // 跳转官方歌单
+  gfGequ(){
+    wx.navigateTo({
+      url: '../gfGequ/gfGequ',
+    })
   }
+
 })
